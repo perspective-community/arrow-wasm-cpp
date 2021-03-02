3bd0b218c181264104c482c1f89674384c5bf810
Tim Paine <t.paine154@gmail.com>
2021-03-01 21:58:18 -0500

    adding files

---
+++ src/emscripten.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2019, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+
+#include <emscripten.h>
+#include <emscripten/bind.h>
+#include <emscripten/val.h>
+
+using namespace emscripten;
+
+/******************************************************************************
+ *
+ * Embind
+ */
+EMSCRIPTEN_BINDINGS(arrow) {
+
+}
