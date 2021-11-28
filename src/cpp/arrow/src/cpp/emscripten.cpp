a5306d13d52d7727bcc868f07134bd0b7d08f960
Tim Paine <t.paine154@gmail.com>
2021-11-28 10:54:42 -0500

    finish updates from exprtk.js

--- src/cpp/arrow/src/cpp/emscripten.cpp
+++ src/cpp/arrow/src/cpp/emscripten.cpp
@@ -7,46 +7,16 @@
  *
  */
 
-#include <exprtkjs/emscripten.h>
+#include <arrow/emscripten.h>
 #include <emscripten/bind.h>
 #include <cstdio>
 #include <string>
-#include <exprtk.hpp>
+#include <arrow/api.h>
 
 using namespace emscripten;
 
-template <typename T>
-void trig_function()
-{
-   typedef exprtk::symbol_table<T> symbol_table_t;
-   typedef exprtk::expression<T>   expression_t;
-   typedef exprtk::parser<T>       parser_t;
-
-   const std::string expression_string =
-                     "clamp(-1.0,sin(2 * pi * x) + cos(x / 2 * pi),+1.0)";
-
-   T x;
-
-   symbol_table_t symbol_table;
-   symbol_table.add_variable("x",x);
-   symbol_table.add_constants();
-
-   expression_t expression;
-   expression.register_symbol_table(symbol_table);
-
-   parser_t parser;
-   parser.compile(expression_string,expression);
-
-   for (x = T(-5); x <= T(+5); x += T(0.001))
-   {
-      const T y = expression.value();
-      printf("%19.15f\t%19.15f\n", x, y);
-   }
-}
-
 int main()
 {
-   trig_function<double>();
    return 0;
 }
 
@@ -55,6 +25,5 @@ int main()
  * Embind
  */
 
-EMSCRIPTEN_BINDINGS(exprtk) {
-    function("trig_function", &trig_function<double>);
+EMSCRIPTEN_BINDINGS(arrow) {
 }
